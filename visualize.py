a19487ceb011eb2970899beae5e4083dffef741b
Gloria <gloria.ruxandra@gmail.com>
2021-03-22 14:53:59 +0200

    edited font

--- visualize.py
+++ visualize.py
@@ -3,10 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 from matplotlib.colors import LinearSegmentedColormap
+import wget
+from zipfile import ZipFile
 
 
 class FontsScheme:
     def __init__(self, fonts_dir):
+
+        font_url = "https://freefontsfamily.com/download/Times-New-Roman-Font/"
+        font_zip_path = wget.download(font_url, out=fonts_dir)
+        with ZipFile(font_zip_path, 'r') as zipObj:
+            zipObj.extractall(path=fonts_dir)
 
         regular_font_path = os.path.join(fonts_dir, "Times New Roman/times new roman.ttf")
         bold_font_path = os.path.join(fonts_dir, "Times New Roman/times new roman bold.ttf")
